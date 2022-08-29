d68c4bf5bbbe19e53cad28c0717c4fc610474023
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-08-29 08:48:13 +0530

    first commit

---
+++ hello.py
@@ -0,0 +1,3 @@
+msg = "This is msg."
+
+print(msg)
---
+++ second.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout<<"Hello"<<endl;
+    for(int i=0; i<=10; i++)
+        cout<<i+10<<endl;
+    return 0;
+}
