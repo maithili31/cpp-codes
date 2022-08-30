53ae65c58151cb66c119f412d20da4377c10001e
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-08-30 11:09:36 +0530

    <elseif>

---
+++ elseif.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int savings;
+    cin>>savings;
+
+    if (savings>5000) {
+        cout<<"neha\n";
+    } else {
+        cout<<"rashmi\n";
+    }
+    return 0 ;
+
+
+
+
+
+
+}
