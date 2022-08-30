c41758ec0e3dbcd23040aa45e96cede26b6e16fa
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-08-30 23:00:23 +0530

    <loops>

--- loop.cpp
+++ loop.cpp
@@ -5,9 +5,9 @@ int main(){
     int n;
     cin>>n;
 
-    int sum=0;
-    for (int counter=1;counter<=n;counter++){
-        sum=sum+counter ;
+    while (n>0) {
+        cout<<n<<endl;
+        cin>>n;
     }
 
     
