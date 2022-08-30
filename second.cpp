5d5ff8c02d1c31b4d10a4ec235bdfae959309265
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-08-30 11:02:27 +0530

    <cin>

--- second.cpp
+++ second.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 
 int main(){
-    cout<<"Hello world\n";
+    int amount1;
+    cin>>amount1 ;
+
+    int amount2;
+    cin>>amount2;
+
+    int sum=amount1+amount2;
+
+    cout<<sum;
+
+
     return 0;
 }
