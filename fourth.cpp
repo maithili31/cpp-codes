b804aa2b22e73b694f7fb10c78de6d3fde594fe7
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-08-30 10:32:22 +0530

    <primitivedatatype>

--- fourth.cpp
+++ fourth.cpp
@@ -2,8 +2,22 @@
 using namespace std;
 
 int main(){
-    cout<<"Hello"<<endl;
-    for(int i=0; i<=10; i++)
-        cout<<i<<endl;
+
+    int a;
+    a=12;
+
+    cout<<"size of int "<<sizeof(a)<<endl;
+
+    float b;
+    cout<<"size of float "<<sizeof(b)<<endl;
+
+    char c;
+    cout<<"size of char "<<sizeof(c)<<endl;
+
+    bool d;
+    cout<<"size of bool"<<sizeof(d)<<endl;
+
+
+
     return 0;
 }
