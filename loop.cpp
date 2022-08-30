48d642d66d28cd456f92ffd3d1deeec9a9531a02
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-08-30 13:32:10 +0530

    <loops>

---
+++ loop.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    int sum=0;
+    for (int counter=1;counter<=n;counter++){
+        sum=sum+counter ;
+    }
+
+    
+}
