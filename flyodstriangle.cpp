695729988ab240f8532d9addfbac8e35e6ba3b79
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-09-01 21:43:30 +0530

    <floydnumbers>>

--- flyodstriangle.cpp
+++ flyodstriangle.cpp
@@ -8,13 +8,14 @@ int main()
     int count=1;
 
     for (int i=1;i<=n;i++) {
-        for (int j=i; j<=i;j++){
+        for (int j=1; j<=i;j++){
             cout<<count<<" ";
             count++;
 
         }
+        cout<<endl;
     }
-    cout<<endl;
+    
 
 
 
