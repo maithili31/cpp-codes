14aace5012ca728d0b418440843e76a40d59d8f8
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-09-01 09:25:41 +0530

    <calculator>

---
+++ calculator.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n1 , n2 ;
+    cout<<"input two numbers:"<<endl;
+    cin>>n1>>n2;
+
+    char op;
+    cout<<"input an operator :"<<endl;
+    cin>>op;
+
+    switch (op)
+    {
+    case '+':
+        cout<<n1+n2<<endl;
+        break;
+    case '-':
+        cout<<n1-n2<<endl;
+        break;
+    case '%':
+        cout<<n1%n2<<endl;
+        break;
+    case '*':
+        cout<<n1*n2<<endl;
+        break;
+    
+    default:
+        cout<<"not defined"<<endl;
+        break;
+    }
+
+
+
+
+}
