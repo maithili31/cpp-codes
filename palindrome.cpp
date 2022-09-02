08988f0187d96ab6c4ce2a01f1ca907f3c9fd8a9
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-09-02 13:17:21 +0530

    <gadbadhigadbad>

--- palindrome.cpp
+++ palindrome.cpp
@@ -7,22 +7,15 @@ int main ()
     cin>>n;
 
     for (int i=1;i<=n;i++){
-        int j;
-        for (j=1;j<=n-i;j++){
+        for (int j=1;j<=n-i;j++){
             cout<<"  ";
         }
-        int k=i;
-        for (;j<=n;j++){
-            cout<<k--<<"  ";
-        }
-        k=2;
-        for (;j<=n+i-1;j++){
-            cout<<k++<<"  ";
+        for (int j=i;j<=n;j++){
+            cout<<" * ";
         }
         cout<<endl;
-
-
     }
+   
 
 
 
