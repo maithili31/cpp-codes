28f6ee97e912d740ea7f68b2a2f641fa5da03729
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-09-02 08:35:43 +0530

    <using numbrs>

---
+++ invertedpattern.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main () 
+{
+    int row, col;
+    cin>>row>>col;
+    int count;
+
+    for (int i=1; i<=row;i++){
+        for (int j=i;j<=col;j++){
+            cout<<count;
+        }
+        cout<<endl;
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
