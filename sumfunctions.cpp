08fddbd6ade5e5ea62348ed7cc694b1e484de240
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-09-04 18:30:51 +0530

    <usingfunc>

---
+++ sumfunctions.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int sum(int n) {
+    int ans=0;
+    for (int i=1;i<=n;i++)
+    ans= ans+i;
+    return ans;
+}
+
+int main () {
+    int n;
+    cin>>n;
+
+    cout<<sum(n)<<endl;
+
+}
