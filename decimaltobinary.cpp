9788ae01bbe8b78544774263493abbdd7f16cb25
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-09-05 11:28:45 +0530

    <error>

--- decimaltobinary.cpp
+++ decimaltobinary.cpp
@@ -5,11 +5,11 @@ using namespace std;
 int decimaltobinary (int n) {
     int ans=0;
     int x=1;
-    while (x<=n) {
-        
-        ans = ans*10 + (n%10);
-        x *= 2;
-        n /= 2;
+    while (x>0) {
+        int lastdigit= n/x;
+        n -= lastdigit*x;
+        x /=2 ;
+        ans = ans*10 + lastdigit ;
     }
     return ans;
 }
