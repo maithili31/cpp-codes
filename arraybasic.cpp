c04cacae7416f00a906111d77512f7c474cf03a4
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-09-06 21:26:16 +0530

    <arraybasicuse>

---
+++ arraybasic.cpp
@@ -0,0 +1,17 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+    int n;
+    cin>>n;
+
+    int array[n];
+
+    for (int i=0;i<=n-1;i++) {
+        cin>>array[i];
+    }
+    for (int i=0;i<=n-1;i++) {
+        cout<<array[i]<<" ";
+    }
+}
--- decimaltobinary.cpp
+++ decimaltobinary.cpp
@@ -3,15 +3,8 @@
 using namespace std;
 
 int decimaltobinary (int n) {
-    int ans=0;
-    int x=1;
-    while (x>0) {
-        int lastdigit= n/x;
-        n -= lastdigit*x;
-        x /=2 ;
-        ans = ans*10 + lastdigit ;
-    }
-    return ans;
+    
+    
 }
 
 int main () 
