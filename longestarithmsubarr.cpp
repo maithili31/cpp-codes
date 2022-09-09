7796181e1d06cd80dee637fd0fecdfb0e6f3d988
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-09-09 19:34:40 +0530

    <googlekickstarterror>

--- longestarithmsubarr.cpp
+++ longestarithmsubarr.cpp
@@ -10,24 +10,7 @@ int main () {
         cin>>arr[i];
     }
 
-    int ans=2;
+    
     int pd= arr[1]-arr[0];
-
-    int j=2;
-    int current=2;
-
-    while (j<n) {
-        if (pd== arr[j]-arr[j-1]) {
-            current++;
-        }
-        else { 
-            pd= arr[j]-arr[j-1];
-            current =2;
-        }
-        ans= max(ans,current);
-        j++;
-
-    }
-    cout<<ans<<endl;
     return 0;
 }
