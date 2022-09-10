0af7bc8973e90ef92653b099a6567f2ed875af2d
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-09-10 11:29:57 +0530

    <errors>

--- searchingmatrix.cpp
+++ searchingmatrix.cpp
@@ -24,12 +24,18 @@ int main () {
     int x;
     cin>>x;
 
+    bool flag=false;
     for (int i=0;i<n;i++) {
         for (int j=0;j<m;j++) {
             if (arr[i][j]==x) {
-                cout<<"Element found \n";
-            } else 
-            { cout <<" Element not found \n";}
+                cout<<i<<" "<<j<<endl;
+                flag=true;
         }
+    }
+    if (flag) {
+        cout<< " Element is found \n";
+    } else {
+        cout<<"Element is not found \n";
+    }
     }
 }
