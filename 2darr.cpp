807d61742bfc7166bd7deda4bc91bdb98ccd1ce0
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-09-10 10:21:47 +0530

    <matrixbasics>

--- 2darr.cpp
+++ 2darr.cpp
@@ -8,12 +8,18 @@ int main () {
     int arr[n][m];
 
     for (int i=0;i<n;i++) {
-        for (int j=0;j<n;j++) {
+        for (int j=0;j<m;j++) {
             cin>>arr[i][j];
-            cout <<arr[i][j]<<"  ";
         }
     }
+    cout<<" Matrix is : \n " ;
+
+    for (int i=0;i<n;i++) {
+        for (int j=0;j<m;j++) {
+            cout<<arr[i][j]<<" ";
+        }
+        cout << " \n ";
+    }
     
-    cout<<" \n ";
     return 0;
 }
