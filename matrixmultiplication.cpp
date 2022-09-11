9f35f166f1f77cbd3873c84a6e71832441018e55
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-09-11 20:45:13 +0530

    <error>

--- matrixmultiplication.cpp
+++ matrixmultiplication.cpp
@@ -22,11 +22,11 @@ int main () {
 
     int ans[n1][n3];
     for (int i=0;i<n1;i++) {
-        for (int j=0;j<n3;j++) {
+        for (int j=0;j<n2;j++) {
             ans[i][j]=0;
         }
     }
-    
+
     for (int i=0;i<n1;i++) {
         for (int j=0;j<n3;j++) {
             for  (int k=0;k<n2;k++) {
