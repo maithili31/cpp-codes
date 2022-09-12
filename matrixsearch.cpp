b25f93a1e064c23cc86b748f9599a13480b09aea
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-09-12 09:13:00 +0530

    <basics>

---
+++ matrixsearch.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+    int n,m;
+    cin>>n>>m;
+    int find ;
+    cin>>find;
+
+    int mx[n][m];
+
+    for (int i=0;i<n;i++) {
+        for (int j=0;j<m;j++) {
+            cin>>mx[i][j];
+        }
+    }
+
+    int r=0 ;
+    int c=m-1;
+    bool found=false;
+    while (r<n && c>=0) {
+        if (mx[r][c]==find) {
+            found=true;
+        }
+        if (mx[r][c]>find) {
+            c--;
+        } else { r++;}
+    }
+     if (found) {
+        cout<<"element is found";
+     } else 
+     { cout<<"element not found";}
+
+    cout<<endl;
+
+    return 0;
+}
