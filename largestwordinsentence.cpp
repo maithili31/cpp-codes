f3dd95cda388fc0a20c6d058726d19f52a84bca8
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-09-12 20:02:36 +0530

    <error>

---
+++ largestwordinsentence.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+    int n;
+    cin>>n;
+    char arr[n+1];
+
+    cin.getline(arr,n);
+    cin.ignore();
+
+    int i=0;
+    int currentlength=0;
+    int maxlength=0;
+
+    while (1) {
+        if (arr[i]== ' ' || arr[i]== '\0') {
+            if (currentlength>maxlength) {
+                maxlength=currentlength;
+            }
+            currentlength=0;
+        }
+        else 
+        {currentlength++;}
+        
+        if (arr[i]=='\0')
+        break;
+    }
+
+    
+    cout<<maxlength<<endl;
+    return 0;
+}
