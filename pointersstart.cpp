da6e5c7a1fbf33a565424a11b9882aed9407f6a1
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-09-14 10:54:44 +0530

    <pointernarr>

--- pointersstart.cpp
+++ pointersstart.cpp
@@ -5,7 +5,11 @@ int main () {
     int a=10;
     int *apointer= &a;
 
-    cout<<apointer<<endl;
-    apointer++;
-    cout<<apointer<<endl;
+    cout<<*apointer<<endl;
+    *apointer=20;
+    cout<<a<<endl;
+
+    int arr[]={10,20,30};
+    cout<<*arr<<endl;
+
 }
