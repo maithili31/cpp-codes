06e95f4de7f650e24d26571e77376554f310836a
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-09-15 08:48:48 +0530

    <basic>

--- swap.cpp
+++ swap.cpp
@@ -1,16 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void swap(int a , int b)
+void swap(int *a , int *b)
 {
-    int temp=a;
-    a=b;
-    b=temp;
+    int temp=*a;
+    *a=*b;
+    *b=temp;
 }
 int main () {
     int a=2;
     int b=4;
 
-    swap (a,b);
+    int *apointer=&a;
+    int *bpointer=&b;
+
+    swap (apointer,bpointer);
     cout<<a<<" "<<b<<endl;
 }
