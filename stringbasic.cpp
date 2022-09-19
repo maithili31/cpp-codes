5ce33a406ae339df01e32c6dd8461e11c24a5b5a
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-09-19 08:39:13 +0530

    <basic>

--- stringbasic.cpp
+++ stringbasic.cpp
@@ -3,14 +3,11 @@ using namespace std;
 
 int main (){
 
-   string str;
-   cin>>str;
-   cout<<str; 
-
+    string str;
+    getline(cin,str);
+    cout<<str<<endl;
     
-   string str1(5,'n') ;
-
-   string str2="rahul";
-   cout<<str2;
+    
+    
     return 0;
 }
