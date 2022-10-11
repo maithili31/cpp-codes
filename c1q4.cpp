875cb491a8d48f27ea6ce0c70396b921f5342bcc
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-10-11 11:31:52 +0530

    chptr1

---
+++ c1q4.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+    // convert fahrenheit to celsius.
+    float x;
+    cin>>x;
+    float y=(5*(x-32))/9;
+    cout<<y<<endl;
+    return 0;
+}
