19b68cf792d1799b6de777c447c0ae7937e035a3
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-10-11 09:24:37 +0530

    chapter1

---
+++ c1q3.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+    int a,b,c,d,e;
+    cin>>a>>b>>c>>d>>e;
+    int f=a+b+c+d+e;
+    cout<<f<<endl;
+    float i=(f/5);
+    cout<<i<<endl;
+    return 0;
+}
