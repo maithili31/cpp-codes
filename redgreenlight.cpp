2ccbef9d23bef3878830b0d328b95231683a5b5c
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-10-14 21:26:19 +0530

    practice

--- redgreenlight.cpp
+++ redgreenlight.cpp
@@ -2,20 +2,19 @@
 using namespace std;
 
 int main() {
-	// your code goes here
 	int t;
 	cin>>t;
-	for (int i=1;i<=t;i++){
+	while (t--) {
 	    int count=0;
 	    int n,k;
 	    cin>>n;
 	    cin>>k;
 	    int h[n];
-	    for (int j=0;j<n;j++){
-	        cin>>h[j];
+	    for (int i=0;i<n;i++){
+	        cin>>h[i];
 	    }
-	    for (int j=0;j<n;j++){
-	        if(h[j]>k)
+	    for (int i=0;i<n;i++){
+	        if(h[i]>k)
 	        {
 	            count++;
 	        }
