04a2e8e02f0882f3a4ef0e3e9c4e2ac57eaad1f9
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-10-15 07:02:20 +0530

    chpter2

---
+++ c2q1.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+    int a[5];
+    int sum=0;
+    for (int i=0;i<5;i++) {
+        cin>>a[i];
+        sum+=a[i];
+    }
+    cout<<sum<<endl;
+    return 0;
+}
