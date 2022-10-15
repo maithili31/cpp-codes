711cc98c841985f204c7c6dc21c47168f5658c85
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-10-15 07:28:47 +0530

    letusC

---
+++ c2q6.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+    int x;
+    cin>>x;
+    cout<<sin(x)<<"  "<<cos(x)<<"  "<<tan(x)<<"  "<<endl;
+    cout<<pow(sin(x),-1)<<"  "<<pow(cos(x),-1)<<"  "<<pow(tan(x),-1)<<endl;
+    return 0;
+}
