d7d7b5522bd156a733ad5ddfd82733224ee269d1
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-10-15 19:53:36 +0530

    letusC

---
+++ c3q7.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+    int a,b,c;
+    cin>>a>>b>>c;
+    int d=a+b+c;
+    if (d==180) {
+        cout<<"yes"<<endl;
+    } else {
+        cout<<"no"<<endl;
+    }
+    return 0;
+}
