2039f9306207a34a95337aeb1cab073a95ffda3b
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-10-15 07:30:49 +0530

    interchange-letusC

---
+++ c2q7.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+    int c,d;
+    cin>>c>>d;
+    int t=0;
+    t=c;
+    c=d;
+    d=t;
+    cout<<c<<"  "<<d<<endl;
+    return 0;
+}
