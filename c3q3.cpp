79758c8cd1bb7f7fbf2a2c6a20963a55dfc0eb86
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-10-15 10:32:37 +0530

    letusC

---
+++ c3q3.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+    int x;
+    cin>>x;
+    if (x%4==0) {
+        cout<<"leap year"<<endl;
+    } else {
+        cout<<"not leap yr"<<endl;
+    }
+    return 0;
+}
