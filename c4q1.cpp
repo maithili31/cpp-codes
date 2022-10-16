4d5ad7c459cf255d4234ed2c53fb9952e8626ba2
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-10-16 18:40:19 +0530

    nouseofoperators-check

---
+++ c4q1.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+    int n;
+    cin>>n;
+    if (n%4==0) {
+        cout<<"yes"<<endl;
+    } else {
+        cout<<"no"<<endl;
+    }
+    return 0;
+}
