d925eced0339808887f31f9853b8053a73a85a5b
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-10-18 11:31:19 +0530

    codechef

--- chefjudge.cpp
+++ chefjudge.cpp
@@ -2,33 +2,40 @@
 using namespace std;
 
 int main() {
-	// your code goes here
 	int t;
 	cin>>t;
 	while(t--){
 	    int n;
 	    cin>>n;
-	    int Alice[n]={0};
-	    int Bob[n]={0};
-	 long long int maxA=0,sumA=0;
-	  long long int maxB=0,sumB=0;
+	    int a[n]={0};
+	    int b[n]={0};
+	    int maxa=0,suma=0;
+	    int maxb=0,sumb=0;
 	    for(int i=0;i<n;i++){
-	        cin>>Alice[i]; //input in array
-	        sumA=sumA+Alice[i];//total sum of alice 
-	        if(Alice[i]>maxA){maxA=Alice[i];}//max time for alice 
+	        cin>>a[i];
+	        suma=suma+a[i]; 
+	        if(a[i]>maxa){
+                maxa=a[i];
+            }
 	    }
 	    for(int i=0;i<n;i++){
-	        cin>>Bob[i];//input in array
-	        sumB=sumB+Bob[i];//total sum of bob 
-	        if(Bob[i]>maxB){maxB=Bob[i];}//max time for bob
+	        cin>>b[i];
+	        sumb=sumb+b[i]; 
+	        if(b[i]>maxb){
+                maxb=b[i];
+            }
 	    }
-	    //remove maximum time of all time 
-	    sumA=sumA-maxA;
-	    sumB=sumB-maxB;
-	  //  output
-	    if(sumB>sumA){cout<<"Alice"<<endl;}
-	    else if (sumA>sumB){cout<<"Bob"<<endl;}
-	    else{cout<<"Draw"<<endl;}
+	    suma=suma-maxa;
+	    sumb=sumb-maxb;
+	    if(sumb>suma){
+            cout<<"Alice"<<endl;
+        }
+	    else if (suma>sumb){
+            cout<<"Bob"<<endl;
+        }
+	    else{
+            cout<<"Draw"<<endl;
+        }
 	}
 	return 0;
 }
