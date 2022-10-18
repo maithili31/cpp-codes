df2df4fb171f02d0d95634157122e3992cc9f1fa
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-10-18 19:55:23 +0530

    codechef

--- joiningdate.cpp
+++ joiningdate.cpp
@@ -4,22 +4,18 @@ using namespace std;
 int main() {
 	int t;
 	cin>>t;
-	while(t--)
-	{
+	while(t--){
 	    int n,k;
 	    cin>>n>>k;
 	    int a=n/5;
-	    if(n%5!=0)
-	    {
+	    if(n%5!=0){
 	        a++;
 	    }
 	    int b=k/5;
-	    if(k%5!=0)
-	    {
+	    if(k%5!=0){
 	        b++;
 	    }
 	    cout<<a-b<<endl;
-	    
 	}
 	return 0;
 }
