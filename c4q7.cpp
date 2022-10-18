c949594c5e14fbebbde6f0117527f393dd564300
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-10-18 20:06:17 +0530

    letusC

--- c4q7.cpp
+++ c4q7.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 
 int main () {
-    
+    int n;
+    cin>>n;
+    int a= sin(n);
+    int b= cos(n);
+    int c= a*a+b*b;
+    int d=pow(c,0.5);
+    if (d==1) {
+        cout<<"yes"<<endl;
+    } else {
+        cout<<"no"<<endl;
+    }
     return 0;
 }
