c38b956e620a69a0c52dbea67d9f82a084f82e9f
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-10-18 20:01:10 +0530

    codechef

--- testcases.cpp
+++ testcases.cpp
@@ -2,25 +2,25 @@
 using namespace std;
 
 int main() {
-	// your code goes here
-	int t;cin>>t;
-	
+	int t;
+    cin>>t;
 	while(t--) {
-	    int n;cin>>n;
+	    int n;
+        cin>>n;
 	    int s[n];
 	    for(int i=0;i<n;i++) {
 	        cin>>s[i];
 	    }
 	    string str;
 	    cin>>str;
-	    int minx = INT_MAX;
+	    int m = INT_MAX;
 	    for(int i=0;i<n;i++) {
 	       if(str[i] == '0'){
-	          minx = min(minx,s[i]); 
+	          m = min(m,s[i]); 
 	       } 
 	    }
 	    
-	    cout<<minx<<endl;
+	    cout<<m<<endl;
 	    
 	}
 	return 0;
