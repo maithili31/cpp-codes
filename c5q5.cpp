52b9dd44f3e7d023b9b86cd7126bc339c825bfd5
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-10-19 19:41:57 +0530

    firulta

--- c5q5.cpp
+++ c5q5.cpp
@@ -5,14 +5,18 @@ int main () {
     int n;
     cin>>n;
     int octal=0,i=1;
-    int x;
+    int x=0;
+    int reverse=0,lastdigits=0;
     while (n!=0) {
         x=n%8;
         n=n/8;
         octal += x*i;
         i *= 10;
-        cout<<x;
+        int lastdigits = x%10;
+        reverse = reverse*10 + lastdigits;
+        x=x/10;
     }
-    cout<<endl;
+    cout<<reverse<<endl;
+    
     return 0;
 }
