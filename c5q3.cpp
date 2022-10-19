0b4c0d688cf74978cd13f5bba0be16e626a9f72b
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-10-19 12:49:27 +0530

    letusC

---
+++ c5q3.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+    int n;
+    cin>>n;
+    int sum=0;
+    int originalno=n;
+    while (n>0)
+    {
+        int lastdigit=n%10;
+        sum= lastdigit*lastdigit*lastdigit + sum;
+        n=n/10;
+    }
+    if (sum==originalno) {
+        cout<<"Armstrong no"<<endl;
+    }
+    else {
+        cout<<"not armstrong no"<<endl;
+    }
+    return 0;
+}
