a8bcf51aae9627276ccf25031d114b7969f23927
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-10-20 16:17:10 +0530

    letusC

---
+++ c6q5.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a,b,flag;
+    cin>>a>>b;
+        for (int i=a;i<=b;i++) {
+            if (i==1 || i==0) {
+                continue;
+            }
+            flag=1;
+            for (int j=2;j<=i/2;++j) {
+                if (i%j==0) {
+                    flag=0;
+                    break;
+                }
+            }
+            if (flag==1) {
+                cout<<i<<"  "<<endl;
+            }
+        }
+    return 0;
+}
