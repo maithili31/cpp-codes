6762e5693346f5b8499cfe246b09bd2cb53bf603
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-10-21 09:16:40 +0530

    firgalat

--- c7q11.cpp
+++ c7q11.cpp
@@ -2,38 +2,37 @@
 using namespace std;
 
 int main () {
-    int n=2;
-    while (n!=0) {
-        switch (n) {
-            case 1 :
-                cout<<"factorial of a number"<<endl;
-                 int factorial =1 ;
-                for (int i=2; i<=n;i++) {
-                    factorial = factorial*i ;
-                }
-                cout<<factorial<<endl;
-                break;
-            
-            case 2 :
-                for (int i=0;i<=sqrt(n);i++) {
-                    if (n%i==0) {
-                        cout<<"not prime"<<endl;
-                        break;
-                    } else {
-                        cout<<"prime"<<endl;
-                    }
-                }
-                break;
-            case 3 :
-                if (n%2==0) {
-                    cout<<"even number"<<endl;
-                } else {
-                    cout<<"odd number"<<endl;
-                }
-                break;
-            default :
-                exit;
+    int n,n1;
+    cin>>n>>n1;
+    if (n=='1') {
+        cout<<"factorial of a number"<<endl;
+        int fact1=1;
+        for (int i=1;i<=n1;i++){
+            fact1=fact1*i;
         }
+        cout<<fact1<<endl;
+    }
+    if (n=='2') {
+        cout<<"prime or not"<<endl;
+        for (int i=1;i<=sqrt(n1);i++) {
+            if (n1%i==0) {
+                cout<<"not prime"<<endl;
+                break;
+            } else {
+                cout<<"prime number"<<endl;
+            }
+        }
+    }
+    if (n=='3') {
+        cout<<"odd or even"<<endl;
+        if (n1%2==0) {
+            cout<<"even"<<endl;
+        } else {
+            cout<<"odd"<<endl;
+        }
+    }
+    if (n=='4') {
+        cout<<"done deal"<<endl;
     }
     return 0;
 }
