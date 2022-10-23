486fd09cbbb6d55383cc75350ad4a0fc1f7b2241
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-10-23 09:54:47 +0530

    letusC

---
+++ c9q9.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int school (int a,int b, int c) {
+    float avg= (a+b+c)/3;
+    cout<<avg<<"  ";
+    float pp= (a+b+c)/3;
+    cout<<pp<<"  ";
+    cout<<endl;
+    return 0;
+}
+
+int main () {
+    int a,b,c;
+    cin>>a>>b>>c;
+    int x=school(a,b,c);
+    cout<<x<<endl;
+    return 0;
+}
