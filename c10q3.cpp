a6dda346095d753f32a2f02eff3726c3a133923a
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-10-24 22:30:22 +0530

    letusC

---
+++ c10q3.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+    int i=0;
+    i++;
+    if (i<=5) {
+        printf ("c adds wings to your thoughts\n");
+        exit (0);
+        main ();
+    }
+    return 0;
+}
