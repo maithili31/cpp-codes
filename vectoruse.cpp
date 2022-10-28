3c3af6ca3437db1e5afcaa160080d718b7bbffe1
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-10-28 20:30:10 +0530

    basic

--- vectoruse.cpp
+++ vectoruse.cpp
@@ -7,15 +7,14 @@ int main (){
     v.push_back (20);
     v.push_back(30);
     v[1]=100;
-    v[4]=2002;
-    v[5]=101;
     v.push_back(23);
     v.push_back(234);
+
+    v.pop_back ();
 
     for (int i=0;i<v.size();i++) {
         cout<<v[i]<<"  ";
     }
-    cout<<v[5]<<endl;
     cout<<endl;
     return 0;
 }
