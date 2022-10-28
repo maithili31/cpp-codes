a9934b7f77192f4a6111cfc4027ff862da03f580
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-10-28 20:43:34 +0530

    basic

--- vectoruse.cpp
+++ vectoruse.cpp
@@ -5,8 +5,8 @@ int main (){
     vector <int> v; 
 
     for (int i=0;i<100;i++) {
-        cout<<v.capacity() <<endl;
-        cout<<v.size()<<endl;
+        cout<<"capacity ="<<v.capacity() <<endl;
+        cout<<"size ="<<v.size()<<endl;
         v.push_back(i+1);
     }
 
