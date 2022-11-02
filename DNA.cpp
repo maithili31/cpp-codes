9b6d5b0206918dbc09f3c0df53d4f1011e2ec8c7
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-11-02 13:00:17 +0530

    codechef

--- DNA.cpp
+++ DNA.cpp
@@ -2,19 +2,16 @@
 using namespace std;
 
 int main() {
-	// your code goes here
-	int n;
-    cin>>n;
-    for(int i=0;i<n;i++){
+	int t;
+    cin>>t;
+    while (t--){
         int m;
         cin>>m;
         string s;
         cin>>s;
-        m=s.length();
-        for(int i=0;i<m-1;i=i+2){
+        for(int i=0;i<s.length()-1;i=i+2){
             if(s[i]=='0' && s[i+1]=='0'){
                 cout<<"A";
-
             }
             else if(s[i]=='0' && s[i+1]=='1'){
                 cout<<"T";
