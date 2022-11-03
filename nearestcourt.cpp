90feb3f936411d1115e4de6e3caa87530fe6ac21
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-11-03 10:38:14 +0530

    codechef

--- nearestcourt.cpp
+++ nearestcourt.cpp
@@ -2,23 +2,24 @@
 using namespace std;
 
 int main() {
-	// your code goes here
 	int t;
 	cin>>t;
-	for(int i=0;i<t;i++){
+	while (t--){
 	    int a,b;
 	    cin>>a>>b;
 	    if((b-a)%2==0){
-	         if(b>a)
-	        cout<<((b-a)/2)<<endl;
-	        else
-	        cout<<((a-b)/2)<<endl;
+	         if(b>a) {
+                cout<<((b-a)/2)<<endl;
+             } else {
+                cout<<((a-b)/2)<<endl;
+             }
 	    }
-	    else{
-	        if(b>a)
-	        cout<<((b-a)/2)+1<<endl;
-	        else
-	        cout<<((a-b)/2)+1<<endl;
+	    else {
+	        if(b>a) {
+                cout<<((b-a)/2)+1<<endl;
+            } else {
+                cout<<((a-b)/2)+1<<endl;
+            }
 	    }
 	}
 	return 0;
