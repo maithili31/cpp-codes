98a02a1b308ac533f531f82d23f3833826d79619
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-11-03 11:05:09 +0530

    codechef

--- equalisno.cpp
+++ equalisno.cpp
@@ -1,28 +1,17 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-
-
-void sol()
-{
-    int a,b;
-    cin>>a>>b;
-    
-    if((a+b)%2==0)
-    {
-        cout<<"YES"<<endl;
-    }
-    else
-    {
-        cout<<"NO"<<endl;
-    }
-}
 int main() {
-	// your code goes here
 	int t;
 	cin>>t;
-	
-	while(t--)
-	sol();
+	while (t--) {
+	    int a,b;
+	    cin>>a>>b;
+	    if ((a+b)%2==0) {
+	        cout<<"yes"<<endl;
+	    } else {
+	        cout<<"no"<<endl;
+	    }
+	}
 	return 0;
 }
