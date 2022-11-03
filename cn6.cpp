381b9ff998a3743c9225036df16e83996d2db8cf
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-11-03 12:13:09 +0530

    aadhaassignment

--- cn6.cpp
+++ cn6.cpp
@@ -10,6 +10,15 @@ int main () {
             cin>>a[i][j];
         }
     }
-    
+    int sum=0;
+    for (int i=0;i<n;i++) {
+        for (int j=0;j<n;j++) {
+            if (i==j){
+                sum += a[i][j];
+            }
+        }
+    }
+    cout<<sum<<endl;
+
     return 0;
 }
