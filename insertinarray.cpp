e4c9bfc7d128e16e593ac0fdd8de9a094cb2ef56
Maithili Mishra <mishra.maithili2931@gmail.com>
2022-12-29 14:01:57 +0530

    algjghrepeat

--- insertinarray.cpp
+++ insertinarray.cpp
@@ -2,21 +2,19 @@
 using namespace std;
 
 int main () {
-    int n;
-    cin>>n;
+    int n,pos,x;
+    cin>>n>>pos>>x;
     int a[n];
     for (int i=0;i<n;i++) {
         cin>>a[i];
     }
-    int x,pos;
-    cin>>x>>pos;
-    a[pos-1]=x;
     for (int i=n;i>pos;i--) {
         a[i]=a[i-1];
+        a[pos-1]=x;
     }
-    for (int i=0;i<=n;i++) {
+    for (int i=0;i<n;i++) {
         cout<<a[i]<<"  ";
-    
-    }
+    } 
+    cout<<endl;
     return 0;
 }
