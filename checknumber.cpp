7b81e81a7799effd7f371902a6ef2c2bb36e886f
Maithili Mishra <mishra.maithili2931@gmail.com>
2023-01-14 15:50:05 +0530

    practice

---
+++ checknumber.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+    int n;
+    cin>>n;
+    if (n<0) {
+        cout<<-1<<endl;
+    } else if (n>0) {
+        cout<<1<<endl;
+    } else{
+        cout<<0<<endl;
+    }
+    return 0;
+}
