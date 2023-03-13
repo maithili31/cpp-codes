43649b494217e40e09b2cd11970fc8a12704cf85
Maithili Mishra <mishra.maithili2931@gmail.com>
2023-03-13 22:47:17 +0530

    mega

---
+++ assign4.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printage(int age[],int n) {
+    for (int i=0;i<n;i++) {
+        cout<<age[i]<<endl;
+    }
+}
+
+int main () {
+    int age[5]={11,14,15,18,20};
+    printage(age,5);
+    return 0;
+
+}
