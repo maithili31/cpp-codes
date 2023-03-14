a39d04cece5acdfe49043f13356bd2d9ea3b157d
Maithili Mishra <mishra.maithili2931@gmail.com>
2023-03-14 19:32:37 +0530

    mega

--- seclargearray.cpp
+++ seclargearray.cpp
@@ -2,6 +2,22 @@
 using namespace std;
 
 int main () {
-    
+    int n,k;
+    cin>>n;
+    int a[n];
+    for (int i=0;i<n;i++) {
+        cin>>a[i];
+    }
+    for (int i=0;i<n-1;i++) {
+        for (int j=i+1;j<n;j++) {
+            if (a[j]<a[i]) {
+                int k=a[j];
+                a[j]=a[i];
+                a[i]=k;
+            }
+        }
+    }
+    cout<<a[n-2]<<endl;
+   
     return 0;
 }
