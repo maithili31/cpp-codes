28e0a5ce030458dc083aedd21b494937b32683de
Maithili Mishra <mishra.maithili2931@gmail.com>
2023-03-22 19:43:12 +0530

    oops

---
+++ Createobj.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include "Student.cpp"
+
+int main () {
+    Student s1;
+    Student s2;
+
+    Student s3,s4,s5;
+
+    s1.age= 24;
+    s1.rollnumber=101;
+    cout<<s1.age<<endl;
+    cout<<s1.rollnumber<<endl;
+
+    Student *s6= new Student;
+}
