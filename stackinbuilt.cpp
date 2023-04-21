2b074eb323ff62da47e3e94e097e45b59f61a2c2
Maithili Mishra <mishra.maithili2931@gmail.com>
2023-04-22 00:40:29 +0530

    start

--- stackinbuilt.cpp
+++ stackinbuilt.cpp
@@ -17,22 +17,4 @@ int main () {
     cout<<s.size()<<endl;
     cout<<s.empty()<<endl;
 
-    /*
-    StackusingArray s;
-    s.push(10);
-    s.push(20);
-    s.push(30);
-    s.push(40);
-    s.push(50);
-
-    cout<<s.top()<<endl;
-
-    cout<<s.pop()<<endl;
-    cout<<s.pop()<<endl;
-    cout<<s.pop()<<endl;
-
-    cout<<s.size()<<endl;
-    cout<<s.isEmpty()<<endl;
-    return 0;
-    */
 }
