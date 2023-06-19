43a466be70aba0c893499f8a7d15d38e92fb13ae
Maithili Mishra <mishra.maithili2931@gmail.com>
2023-06-19 19:08:05 +0530

    practice

---
+++ reversesentence.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void reversesen(string s) {
+
+    stack <string> st;
+
+    for (int i=0;i<s.length();i++) {
+        string word="";
+        while (s[i]!=' ' && i<s.length() ) {
+            word+=s[i];
+            i++;
+        }
+        st.push(word);
+    }
+    while (!st.empty()) {
+        cout<<st.top()<<"  ";
+        st.pop();
+    }
+    cout<<endl;
+}
+
+int main () {
+
+    string s="Hey how are you doing?";
+    reversesen(s);
+    return 0;
+}
