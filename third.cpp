650b83263a5cdaec76e76eb62bdb165fdcd31d93
Maithili Mishra <mishra.maithili2931@gmail.com>
2023-06-23 20:12:44 +0530

    practice

--- third.cpp
+++ third.cpp
@@ -6,14 +6,18 @@ int main()
     int a,b;
     cin>>a>>b;
 
+    vector<int> v;
     for (int i=a;i<b;i++){
         for (int num= 2; num<=i; num++){
             if (i%num==0) {
                 break;
             } else {
-                cout<<i<<endl;
+                v.push_back(i);
             }
         }
+    }
+    for (int i=0;i<v.size();i++) {
+        cout<<v[i]<<" ";
     }
     return 0;
 }
