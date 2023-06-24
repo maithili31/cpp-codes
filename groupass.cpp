12fba8117e43040738a728f1d2d9c28ec2c785f6
Maithili Mishra <mishra.maithili2931@gmail.com>
2023-06-24 12:36:36 +0530

    practice

---
+++ groupass.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while (t--) {
+	    int n,x;
+	    cin>>n>>x;
+	    int y=(2*n)+1;
+	    cout<<y-x<<endl;
+	}
+	return 0;
+}
