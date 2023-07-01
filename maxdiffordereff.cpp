6ccad90421dd7d6639a661677bfc6c79d62d8673
Maithili Mishra <mishra.maithili2931@gmail.com>
2023-07-01 21:04:54 +0530

    gfgcourse

---
+++ maxdiffordereff.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++) {
+        cin>>a[i];
+    }
+    int maxi=a[1]-a[0];
+    int mini=a[0];
+    for(int i=1;i<n;i++) {
+        if(a[i]-mini>maxi) {
+            maxi=a[i]-mini;
+        }
+        if (a[i]<mini) {
+            mini=a[i];
+        }
+    }
+    cout<<maxi<<endl;
+    return 0;
+}
