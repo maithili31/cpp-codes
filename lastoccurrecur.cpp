8bc8e5ea00f63436e0c48f3c81f915f7fc587c1c
Maithili Mishra <mishra.maithili2931@gmail.com>
2023-07-05 17:55:00 +0530

    gfgcourse

--- lastoccurrecur.cpp
+++ lastoccurrecur.cpp
@@ -1,6 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int lastoccur(int a[],int low , int high , int x,int n) {
+    if (low>high) {
+        return -1;
+    }
+    int mid=(low+high)/2;
+    if(x>a[mid]) {
+        return lastoccur(a,mid+1,high,x,n);
+    } else if (x<a[mid]) {
+        return lastoccur(a,low,mid-1,x,n);
+    } else {
+        if(mid==n-1 || a[mid+1]!=a[mid]) {
+            return mid;
+        } else {
+            return lastoccur(a,mid+1,high,x,n);
+        }
+    }
+}
+
 int main() {
     int n,x;
     cin>>n>>x;
@@ -8,6 +26,6 @@ int main() {
     for(int i=0;i<n;i++){
         cin>>a[i];
     }
-    
+    cout<<lastoccur(a,0,n-1,x,n);
     return 0;
 }
