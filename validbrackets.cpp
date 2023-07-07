0397163240aa8c298e451b6e2675b9ed61a62784
Maithili Mishra <mishra.maithili2931@gmail.com>
2023-07-07 15:25:43 +0530

    cc

--- validbrackets.cpp
+++ validbrackets.cpp
@@ -1,12 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool isValid(string s) {
+    int n=s.size();
+    stack<char> st;
+    bool ans=true;
+    for(int i=0;i<n;i++) {
+        if(s[i]=='(' || s[i]=='{' || s[i]=='[') {
+            st.push(s[i]);
+        }
+        else if (s[i]==')') {
+            if(st.top()=='(') {
+                st.pop();
+            } else {
+                ans=false;
+                break;
+            }
+        }
+        else if (s[i]=='}') {
+            if(st.top()=='{') {
+                st.pop();
+            } else {
+                ans=false;
+                break;
+            }
+        }
+        else if (s[i]==']') {
+            if(st.top()=='[') {
+                st.pop();
+            } else {
+                ans=false;
+                break;
+            }
+        }
+    } 
+    return ans;           
+}
+
 int main() {
     string s;
     cin>>s;
-    for(int i=0;i<s.length();i++) {
-        
-    }
 
+    if(isValid(s)) {
+        cout<<"true"<<endl;
+    } else {
+        cout<<"false"<<endl;
+    }
     return 0;
 }
