34a2511eb498073bd852c34993edc89ae3209663
Maithili Mishra <mishra.maithili2931@gmail.com>
2023-07-11 08:08:10 +0530

    cc

--- chefdiet.cpp
+++ chefdiet.cpp
@@ -2,35 +2,29 @@
 using namespace std;
 
 int main() {
-	// your code goes here
 	int t;
 	cin>>t;
-	while(t--)
-	{
+	while(t--) {
 	    int n,k;
 	    cin>>n>>k;
 	    int a[n];
-	    for(int i=0;i<n;i++)
-	    {
+	    for(int i=0;i<n;i++) {
 	        cin>>a[i];
 	    }
-	    int t=0;
+	    int x=0;
 	    for(int i=0;i<n;i++)
 	    {
-	        if(a[i]>=k)
-	        {
+	        if(a[i]>=k) {
 	            a[i+1]=a[i+1]+a[i]-k;
-	        }
-	        else
-	        {
-	            t = i+1;
+	        } else {
+	            x =i+1;
 	            break;
 	        }
 	    }
-	    if(t==0)
-	      cout<<"YES"<<'\n';
+	    if(x==0)
+	      cout<<"yes"<<endl;
 	    else
-	      cout<<"NO "<<t<<'\n';
+	      cout<<"no"<<t<<endl;
 	}
 	return 0;
 }
