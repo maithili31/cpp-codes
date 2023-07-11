a28094b94557c9d293bb2128ab50ae3274ba70a9
Maithili Mishra <mishra.maithili2931@gmail.com>
2023-07-11 08:02:19 +0530

    cc

--- snake.cpp
+++ snake.cpp
@@ -1,39 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-	int r;
-	cin>>r;
-	while(r--){
+	int t;
+	cin>>t;
+	while(t--){
 	    int l;
 	    cin>>l;
+
 	    string s;
 	    cin>>s;
-	    string sn;
+
+	    string st;
+
 	    for(int i=0;i<l;i++){
 	        if(s[i]!='.')
-	        sn=sn+s[i];
+	        st=st+s[i];
 	    } 
 	    bool ans=true;
-	    if(sn.length()%2!=0)
-	    ans=false;
-	    else
-	    {
-	        for(int i=0;i<sn.length();i++)
-	        {
-	            if(i%2==0)
-	            {
-	                if(sn[i]=='T')
-	                {ans=false;break;}
+	    if(st.length()%2!=0) {
+            ans=false;
+        } else {
+	        for(int i=0;i<st.length();i++) {
+	            if(i%2==0) {
+	                if(st[i]=='T'){
+                        ans=false;
+                        break;
+                    }
 	                
-	            }
-	            else
-	            {
-	                if(sn[i]=='H')
-	               { ans=false;break;}
-	                
+	            } else {
+	                if(st[i]=='H') { 
+                        ans=false;
+                        break;
+                    }
 	            }
 	        }
 	    }
+        
 	    if(ans==true)
 	    cout<<"Valid"<<endl;
 	    else
